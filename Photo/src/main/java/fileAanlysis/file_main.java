9507aa5b0103ff4b268d8ce85f2d68bb2d5c96be
湟 <1182756906@qq.com>
2020-03-19 18:15:31 +0800

    baidu ocr

--- Photo/src/main/java/fileAanlysis/file_main.java
+++ Photo/src/main/java/fileAanlysis/file_main.java
@@ -1,132 +1,76 @@
 package fileAanlysis;
 
 import com.baidu.aip.ocr.AipOcr;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-import com.google.gson.*;
-
-
-public class file_main {
-
-
-    public static Queue<String> queueFilePath=new LinkedList<>();
+public class file_main{
     public static Map<String,String> map_weixin=new HashMap<>();
-    public static ArrayList<String> failed=new ArrayList<>();
+    public static Queue<String> queueFilePath=new LinkedList<>();
+    private static final Path path = Paths.get("E:\\test\\");
+    public static List<String> filePath=new ArrayList<>();
+    public static List<String> failed=new ArrayList<>();
     public static int n=3;
-    public static boolean flag=true;
-
-    public static void main(String [] args)   {
-        final Path path = Paths.get("E:\\test\\");
+    public static void main(String [] args){
         getFileList(path);
-        //先循环遍历一下已经有的文件
-        while (!queueFilePath.isEmpty()){
-            fileanalysis(queueFilePath,n);
+        while(!queueFilePath.isEmpty()){
+            getResult();
+            }
+        for(String map: map_weixin.values()){
+            System.out.println(map);
         }
-        fileScaner(path);
+        System.out.println("Faied"+failed);
     }
-
-
 
     public static void getFileList(Path path) {
         File file = new File(String.valueOf(path));
         File[] tempList = file.listFiles();
         for (int i = 0; i < tempList.length; i++) {
-            if (tempList[i].isFile()) {
+            if (tempList[i].isFile()&&filePath.contains(tempList[i].toString())) {
+                System.out.println(tempList[i].toString());
+                continue;
+            }
+            else{
+                System.out.println(tempList[i].toString());
+                filePath.add(tempList[i].toString());
                 queueFilePath.add(tempList[i].toString());
             }
         }
     }
 
-    public static Path fileScaner(Path path) {
-
-        try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
-            //给path路径加上文件观察服务
-            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
-                    StandardWatchEventKinds.ENTRY_MODIFY,
-                    StandardWatchEventKinds.ENTRY_DELETE);
-            while (true) {
-                final WatchKey key = watchService.take();
-                for (WatchEvent<?> watchEvent : key.pollEvents()) {
-
-                    final WatchEvent<Path> watchEventPath = (WatchEvent<Path>) watchEvent;
-                    final Path filename = watchEventPath.context();
-                    final WatchEvent.Kind<?> kind = watchEvent.kind();
-
-                    if (kind == StandardWatchEventKinds.OVERFLOW) {
-                        continue;
-                    }
-                    //创建事件
-                    if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-
-                        System.out.println("[新建]");
-                        queueFilePath.add(path.toString() + "\\" + filename.toString());
-                        //循环遍历队列，分析队列中的数据
-                        while (!queueFilePath.isEmpty()){
-                            flag=fileanalysis(queueFilePath,n);
-                            if(flag){
-                                n=3;
-                            }
-                            else {
-                                n--;
-                            }
-                        }
-                        //输出结果
-                        for(String value:map_weixin.values())
-                        {
-                            System.out.println(value);
-                        };
-                    }
-
-                    //删除事件
-                    if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-
-                    }
-
-                }
-                boolean valid = key.reset();
-                if (!valid) {
-                    break;
-                }
-            }
-        } catch (InterruptedException | IOException ex) {
-            System.err.println(ex);
-        }
-        return null;
-    }
-
-    public static boolean fileanalysis(Queue<String> Queue_path,int n) {
+    public static void getResult() {
         final String APP_ID = "18890384";
         final String API_KEY = "y50FGh8HekqhbhmALaWVv3qd";
         final String SECRET_KEY = "Bsyq4MMnqGGmOaPd1ylXDTIvUSdq6cKk";
         AipOcr client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
         client.setConnectionTimeoutInMillis(2000);
         client.setSocketTimeoutInMillis(60000);
-        String path = Queue_path.peek();
+        String path = queueFilePath.peek();
         JSONObject res = client.basicGeneral(path, new HashMap<String, String>());
         String analysisjson=analysisJson(res.toString(2));
+        //对分析结果进行判断
         if(analysisjson==null){
-                if(n==0){
-                    String failedFile=queueFilePath.remove();
-                    failed.add(failedFile);
-                    return true;
-                }
-                else {
-                    return false;
-                }
-        }else {
-            map_weixin.put(path,analysisjson );
-            queueFilePath.remove();
-            return true;
+            if(n==0){
+                String failedFile=queueFilePath.peek();
+                failed.add(failedFile);
+                n=3;
+            }
+            else {
+                n--;
+            }
         }
-        //遍历一下结果
-
+        else {
+            map_weixin.put(path,analysisjson );
+            if(!queueFilePath.isEmpty()){
+                queueFilePath.remove();
+            }
+        }
     }
 
     public static String analysisJson(String json) {
@@ -137,13 +81,13 @@ public class file_main {
                 String wordjson = jsonobject.get("words_result").getAsJsonArray().get(i).toString();
                 JsonObject jsonobject2 = (JsonObject) new JsonParser().parse(wordjson);
                 wordRes = wordRes + jsonobject2.get("words").getAsString().trim();
-                //                System.out.println(word_res);
             }
         }catch (Exception e){
             return null;
         }
-                //            System.out.println(word_res);
         return wordRes;
     }
-}
 
+
+
+}
